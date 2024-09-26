34acf23217e31a918af45f4d361e9774a9aa2322
Jonathan Gao <62315101+OPTIMUSGYZ@users.noreply.github.com>
2024-09-26 11:45:46 -0400

    Added a form for username input in index.html using GET and POST

--- hello.py
+++ hello.py
@@ -1,13 +1,22 @@
-import datetime
+from datetime import datetime, timezone
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a random and very long string that is long and long and long and repetitive'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -20,9 +29,14 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.datetime.now(datetime.UTC))
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template('index.html', current_time=datetime.now(timezone.utc), form=form, name=name)
 
 
 @app.route('/user/<name>')
